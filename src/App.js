cde45fc45682654ff337e4fd13b021426b173195
Juan-vargas98 <jortizvargas1603@gmail.com>
2025-09-24 20:30:16 -0500

    Proyecto inicial de la API de películas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import GeneroPage from './pages/GeneroPage.jsx';
+import DirectorPage from './pages/DirectorPage.jsx';
+import ProductoraPage from './pages/ProductoraPage.jsx';
+import TipoPage from './pages/TipoPage.jsx';
+import MediaPage from './pages/MediaPage.jsx';
+import HomePage from './pages/HomePage.jsx'; // Correct import statement
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/generos">Géneros</Link>
+            </li>
+            <li>
+              <Link to="/directores">Directores</Link>
+            </li>
+            <li>
+              <Link to="/productoras">Productoras</Link>
+            </li>
+            <li>
+              <Link to="/tipos">Tipos</Link>
+            </li>
+            <li>
+              <Link to="/media">Media</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="container">
+          <Routes>
+            <Route path="/generos" element={<GeneroPage />} />
+            <Route path="/directores" element={<DirectorPage />} />
+            <Route path="/productoras" element={<ProductoraPage />} />
+            <Route path="/tipos" element={<TipoPage />} />
+            <Route path="/media" element={<MediaPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
